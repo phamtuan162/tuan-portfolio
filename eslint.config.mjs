69137ba4bab49ae2324281ff0ffeec4bd786d52c
phamtuan162 <tnpham352@gmail.com>
2025-04-19 22:41:31 +0700

    update config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,20 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends([
     'next/core-web-vitals',
-    './eslint-config/base.js', // Nhúng cấu hình base
-    './eslint-config/react.js', // Nhúng cấu hình cho React
-    './eslint-config/prettier.js', // Nhúng cấu hình Prettier
+    './eslint-config/base.js', // Đảm bảo các file cấu hình ESLint khác có định dạng chính xác
+    './eslint-config/react.js',
+    './eslint-config/prettier.js',
   ]),
+  {
+    rules: {
+      quotes: ['error', 'double'],
+      'no-console': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-useless-escape': 'off',
+      semi: ['warn', 'always'],
+      'comma-dangle': ['warn', 'only-multiline'],
+    },
+  },
 ];
 
 export default eslintConfig;
