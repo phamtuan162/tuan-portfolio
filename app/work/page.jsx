4d52c6a8dd49e2115afb6cb51b109184e5ec142e
phamtuan162 <tnpham352@gmail.com>
2025-06-23 21:29:45 +0700

    update-code

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { BsArrowUpRight, BsGithub, BsYoutube } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
 import "swiper/css";
@@ -34,6 +34,7 @@ const projects = [
         image: "/assets/work/thumb1.png",
         live: "https://promanage-site.vercel.app/",
         github: "https://github.com/phamtuan162/Project-Trello",
+        youtube: "https://www.youtube.com/watch?v=0t1elPj1m-o&t=26s",
     },
 ];
 
@@ -121,6 +122,24 @@ const WorkPage = () => {
 
                                             <TooltipContent>
                                                 <p>Github repository</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                                {/* demo project button */}
+                                <Link
+                                    href={project?.github || "/"}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <TooltipProvider delayDuration={100}>
+                                        <Tooltip>
+                                            <TooltipTrigger className="w-[48px] h-[48px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                <BsYoutube className="text-white text-xl group-hover:text-accent" />
+                                            </TooltipTrigger>
+
+                                            <TooltipContent>
+                                                <p>Demo youtube</p>
                                             </TooltipContent>
                                         </Tooltip>
                                     </TooltipProvider>
