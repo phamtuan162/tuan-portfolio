1c20432dbebdabff1e47b009fb4ced77332094b0
phamtuan162 <tnpham352@gmail.com>
2025-08-30 01:51:32 +0700

    update-code

--- app/page.jsx
+++ app/page.jsx
@@ -73,7 +73,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <Stats />
+            // <Stats />
         </section>
     );
 }
