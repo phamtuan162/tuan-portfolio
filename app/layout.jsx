992e090ff160adf4800fc7a346b7ab4f9cd537eb
phamtuan162 <tnpham352@gmail.com>
2025-08-29 22:55:24 +0700

    update-code

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,84 +21,21 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                {/* Favicon and Icons */}
-                <link
-                    rel="apple-touch-icon"
-                    sizes="57x57"
-                    href="/assets/favicon/apple-icon-57x57.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="60x60"
-                    href="/assets/favicon/apple-icon-60x60.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="72x72"
-                    href="/assets/favicon/apple-icon-72x72.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="76x76"
-                    href="/assets/favicon/apple-icon-76x76.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="114x114"
-                    href="/assets/favicon/apple-icon-114x114.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="120x120"
-                    href="/assets/favicon/apple-icon-120x120.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="144x144"
-                    href="/assets/favicon/apple-icon-144x144.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="152x152"
-                    href="/assets/favicon/apple-icon-152x152.png"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/assets/favicon/apple-icon-180x180.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="192x192"
-                    href="/assets/favicon/android-icon-192x192.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/assets/favicon/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="96x96"
-                    href="/assets/favicon/favicon-96x96.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/assets/favicon/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/assets/favicon/manifest.json" />
+                {/* Favicon chuẩn */}
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+                {/* Apple Touch Icon */}
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+                {/* Manifest cho PWA */}
+                <link rel="manifest" href="/site.webmanifest" />
+
+                {/* Windows Tile */}
                 <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta
-                    name="msapplication-TileImage"
-                    content="/assets/favicon/ms-icon-144x144.png"
-                />
                 <meta name="theme-color" content="#ffffff" />
             </head>
+
             <body className={jetbrainsMono.variable}>
                 <Header />
                 <main>
